d4543791dd6fb5c4b816f72eea20d94ffc2c31aa
Edi <selimi.edi.1991@gmail.com>
2021-03-24 15:18:19 +0100

    Working on exercise 4

--- src/components/Exercise4.js
+++ src/components/Exercise4.js
@@ -6,13 +6,15 @@ import data from '../data.json'
 
 const Exercise4 = () => {
     const [count, setCount] = useState(0)
+    const [active, setActive] = useState(null)
 
     const newSpeech = () => setCount(count => count + 1)
     const oldSpeech = () => setCount(count => count - 1)
 
-    const [highLight, setHightLight] = useState('')
-    let key = data[count];
-    console.log(data[count])
+    const highLightElement = () => setActive(true)
+
+    console.log(active)
+
     // Style
     const Paragraph = styled.p`background-color: red;`
     const List = styled.ul`
@@ -25,13 +27,21 @@ const Exercise4 = () => {
     padding: .4rem;
     font-weight: 500;
     padding-left: 1rem;
+
+    :active{
+        background-color: red;
+    }
     `;
 
+    const ss = {
+        backgroundColor: '#ccc',
+        fontSize: 24
+    }
     return (
         <div>
             <Button
                 title="Adelante"
-                onClick={newSpeech}
+                onClick={highLightElement}
             />
 
             <Button
@@ -39,7 +49,12 @@ const Exercise4 = () => {
                 onClick={oldSpeech} />
             <List>
                 {data.map((speech, index) => {
-                    return <ListItem key={index}>{speech}</ListItem>
+                    return <ListItem 
+                    key={index}
+                    style={index % index + 1 ? ss : null }
+                   // onChange={ highLightElement}
+                   // style={{color: active.color, height: active.height}}
+                    >{speech}</ListItem>
                 }
                 )}
             </List>
